1f4045d09ec31cc285b131c646dc09e2aaf8c0bf
yun <ziyunn99@gmail.com>
2023-05-28 22:56:14 +0800

    feat: section 5 常見問題 (展開需改善)

--- js/all.js
+++ js/all.js
@@ -1,7 +1,11 @@
 $(".navbar-toggler").click(function () {
   $(".navbar-collapse-list").toggleClass("show");
 });
-
+$(".qa-list> li>a").click(function (event) {
+  event.preventDefault();
+  $(this).find("div").toggleClass("hide-icon");
+  $(this).find("p").toggleClass("show");
+});
 $("#scrollToTop").click(function () {
   $("html, body").animate(
     {
