1e4469c23c24ac5d0111de14ec347ba939603a65
yun <ziyunn99@gmail.com>
2023-05-28 15:49:52 +0800

    feat: scrollToTop

--- js/all.js
+++ js/all.js
@@ -1,3 +1,12 @@
 $(".navbar-toggler").click(function () {
-    $(".navbar-collapse-list").toggleClass("show");
-  });
+  $(".navbar-collapse-list").toggleClass("show");
+});
+
+$("#scrollToTop").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    500
+  );
+});
