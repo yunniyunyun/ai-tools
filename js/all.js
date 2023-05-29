e6790d8bc088778366e789860cd4c0c7bc5d2e77
yun <ziyunn99@gmail.com>
2023-05-29 13:42:04 +0800

    fix: price hover and qa js

--- js/all.js
+++ js/all.js
@@ -4,7 +4,8 @@ $(".navbar-toggler").click(function () {
 $(".qa-list> li>a").click(function (event) {
   event.preventDefault();
   $(this).find("div").toggleClass("hide-icon");
-  $(this).find("p").toggleClass("show");
+  // $(this).find("p").toggleClass("show");
+  $(this).find("p").slideToggle();
 });
 $("#scrollToTop").click(function () {
   $("html, body").animate(
